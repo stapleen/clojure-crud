712588561ad2fb104f13c9304f1e1cf468f645d4
Mikhail <mm.leontev@gmail.com>
2020-09-17 19:55:56 +0300

    add try/catch

--- backend/backend/src/backend/core.clj
+++ backend/backend/src/backend/core.clj
@@ -12,52 +12,60 @@
 
 (defn add-patients
   [request]
-  (let [body (get-in request [:body])
-        full-name (get-in request [:body "full_name"])
-        gender (get-in request [:body "gender"])
-        date-of-birth (get-in request [:body "date_of_birth"])
-        date-of-birth-convert-date (java.sql.Date/valueOf date-of-birth)
-        current-date (.getTime (java.util.Date.))
-        current-date-convert-date (-> current-date java.sql.Timestamp. .toLocalDateTime)]
-
-    (jdbc/insert! db :patients {:full_name full-name
-                                :gender gender
-                                :date_of_birth date-of-birth-convert-date
-                                :created_at current-date-convert-date})
-    (response {:success 1})))
-   
-(defn get-patients [request]
-(let [patients-list (jdbc/query db ["SELECT id, full_name, gender, date_of_birth FROM patients WHERE deleted=false"])]
- (response {:success 1 :result patients-list})))
-
-(defn delete-patient [request]
+  (try
     (let [body (get-in request [:body])
-          patient-id (get-in request [:body "id"])
+          full-name (get-in request [:body "full_name"])
+          gender (get-in request [:body "gender"])
+          date-of-birth (get-in request [:body "date_of_birth"])
+          date-of-birth-convert-date (java.sql.Date/valueOf date-of-birth)
           current-date (.getTime (java.util.Date.))
           current-date-convert-date (-> current-date java.sql.Timestamp. .toLocalDateTime)]
 
-      (jdbc/update! db :patients {:deleted true :updated_at current-date-convert-date} ["id = ?" patient-id])
-      (response {:success 1})))
+      (jdbc/insert! db :patients {:full_name full-name
+                                  :gender gender
+                                  :date_of_birth date-of-birth-convert-date
+                                  :created_at current-date-convert-date})
+      (response {:success 1}))
+    (catch Exception e (response {:success 0 :error "Произошла ошибка"}))))
+   
+(defn get-patients [request]
+(try
+  (let [patients-list (jdbc/query db ["SELECT id, full_name, gender, date_of_birth FROM patients WHERE deleted=false"])]
+    (response {:success 1 :result patients-list}))
+  (catch Exception e (response {:success 0 :error "Произошла ошибка"}))))
 
-(defn update-patient-data [request]
-  (let [body (get-in request [:body])
-        id (get-in request [:body "id"])
-        full-name (get-in request [:body "full_name"])
-        gender (get-in request [:body "gender"])
-        date-of-birth (get-in request [:body "date_of_birth"])
-        date-of-birth-convert-date (if (nil? date-of-birth) nil (java.sql.Date/valueOf date-of-birth))
-        current-date (.getTime (java.util.Date.))
-        current-date-convert-date (-> current-date java.sql.Timestamp. .toLocalDateTime)
-        ]
+(defn delete-patient [request]
+ (try
+   (let [body (get-in request [:body])
+         patient-id (get-in request [:body "id"])
+         current-date (.getTime (java.util.Date.))
+         current-date-convert-date (-> current-date java.sql.Timestamp. .toLocalDateTime)]
 
-    (jdbc/execute! db
-                   ["UPDATE patients SET full_name = COALESCE(?, full_name),
+     (jdbc/update! db :patients {:deleted true :updated_at current-date-convert-date} ["id = ?" patient-id])
+     (response {:success 1}))
+   (catch Exception e (response {:success 0 :error "Произошла ошибка"}))))
+
+(defn update-patient-data
+  [request]
+  (try
+    (let [body (get-in request [:body])
+          id (get-in request [:body "id"])
+          full-name (get-in request [:body "full_name"])
+          gender (get-in request [:body "gender"])
+          date-of-birth (get-in request [:body "date_of_birth"])
+          date-of-birth-convert-date (if (nil? date-of-birth) nil (java.sql.Date/valueOf date-of-birth))
+          current-date (.getTime (java.util.Date.))
+          current-date-convert-date (-> current-date java.sql.Timestamp. .toLocalDateTime)]
+
+      (jdbc/execute! db
+                     ["UPDATE patients SET full_name = COALESCE(?, full_name),
                    gender = COALESCE(?, gender),
                    date_of_birth = COALESCE(?, date_of_birth),
                    updated_at = ?
                    WHERE id = ?" full-name gender date-of-birth-convert-date current-date-convert-date id])
 
-    (response {:success 1})))
+      (response {:success 1}))
+    (catch Exception e (response {:success 0 :error "Произошла ошибка"}))))
 
 (defroutes app
   (POST "/add" [] (-> add-patients middleware/wrap-json-body middleware/wrap-json-response))
