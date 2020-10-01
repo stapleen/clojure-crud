97bb7130eedcd1cfab22d7da40cab53fccc94ba0
Mikhail <mm.leontev@gmail.com>
2020-10-01 16:05:36 +0300

    render patients in table

--- frontend/src/frontend/patients.cljs
+++ frontend/src/frontend/patients.cljs
@@ -8,7 +8,7 @@
 
 (defn component
   []
-  (let [patient (r/atom nil)]
+  (let [patients (r/atom nil)]
 
     (r/create-class
      {:display-name  "patients"
@@ -17,33 +17,40 @@
       (fn [this]
         (go (let [response (<! (http/get "http://localhost:3000/get"))
                   result (get-in response [:body :result])]
-            ;;   (reset! patient result)
-            ;;   (println "patient" patient))))
-              (swap! patient result))))
+              (reset! patients result))))
 
       :reagent-render
       (fn []
+        (let [patients-list
+              (map #(let [id (get-in % [:id])
+                          full-name (get-in % [:full_name])
+                          gender (get-in % [:gender])
+                          date_of_birth (get-in % [:date_of_birth])]
 
-        ;; (mapv #([:div "HI"]) @patient)
-
-        (let [id (get-in @patient [:id])
-              full-name (get-in @patient [:full_name])
-              gender (get-in @patient [:gender])
-              date_of_birth (get-in @patient [:date_of_birth])]
-
+                      [:tr
+                       [:td full-name]
+                       [:td gender]
+                       [:td date_of_birth]
+                       [:td [:input {:type "button"
+                                     :value "Редактировать"
+                                     :on-click (fn [] (set! (.. js/document -location -href) "#/edit/10"))}]]
+                       [:td [:input {:type "button"
+                                     :value "Удалить"
+                                     :on-click 
+                                     (fn []
+                                       (go (let [response (<! (http/post "http://localhost:3000/delete"
+                                                                         {:json-params {:id id}}))
+                                                 success (get-in response [:body :success])
+                                                 result (if (zero? success) (get-in response [:body :error]) (get-in response [:body :result]))]
+                                             (println "response" result))))}]]])
+                   @patients)]
           [:div
            [:input {:type "button" :value "Добавить пациента" :on-click (fn [] (set! (.. js/document -location -href) "#/new"))}]
-
-           [:div
-            [:input {:type "button" :value "Редактировать" :on-click (fn [] (set! (.. js/document -location -href) "#/edit/10"))}]
-            [:input {:type "button"
-                     :value "Удалить"
-                     :on-click (fn []
-                                 (go (let [response (<! (http/post "http://localhost:3000/delete"  {:json-params {:id id}}))
-                                           success (get-in response [:body :success])
-                                           result (if (zero? success) (get-in response [:body :error]) (get-in response [:body :result]))]
-                                       (println "response" result))))}]]
-
-           [:p "full-name: " full-name]
-           [:p "gender: " gender]
-           [:p "date_of_birth: " date_of_birth]]))})))
+           [:table
+            [:tr
+             [:th "Полное имя"]
+             [:th "Пол"]
+             [:th "Дата рождения"]
+             [:th ""]
+             [:th ""]]
+            patients-list]]))})))
