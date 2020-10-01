866e28d935aaeadb043f6d03ac42f6ccbe60c7f1
Mikhail <mm.leontev@gmail.com>
2020-10-01 19:17:21 +0300

    render [] list

--- frontend/src/frontend/components/patients.cljs
+++ frontend/src/frontend/components/patients.cljs
@@ -33,23 +33,35 @@
                        [:td date_of_birth]
                        [:td [:input {:type "button"
                                      :value "Редактировать"
-                                     :on-click (fn [] (set! (.. js/document -location -href) (str "#/edit/" id)))}]]
+                                     :on-click (fn []
+                                                 (set! (.. js/document -location -href) (str "#/edit/" id)))}]]
                        [:td [:input {:type "button"
                                      :value "Удалить"
-                                     :on-click 
+                                     :on-click
                                      (fn []
                                        (go (let [response (<! (http/post "http://localhost:3000/delete"
                                                                          {:json-params {:id id}}))
                                                  success (get-in response [:body :success])]
-                                             (if (zero? success) (println (get-in response [:body :error])) (reset! patients (filterv (fn [x] (not= (get-in x [:id]) id)) @patients))))))}]]])
+                                             (if (zero? success) 
+                                             (println (get-in response [:body :error])) 
+                                             (reset! patients
+                                                     (filterv
+                                                      (fn [x] (not= (get-in x [:id]) id)) @patients))))))}]]])
                    @patients)]
-          [:div
-           [:input {:type "button" :value "Добавить пациента" :on-click (fn [] (set! (.. js/document -location -href) "#/new"))}]
-           [:table
-            [:tr
-             [:th "Полное имя"]
-             [:th "Пол"]
-             [:th "Дата рождения"]
-             [:th ""]
-             [:th ""]]
-            patients-list]]))})))
+
+
+
+          (if (empty? patients-list)
+            [:p "Список пациентов пуст"]
+            [:div
+             [:input {:type "button"
+                      :value "Добавить пациента"
+                      :on-click (fn [] (set! (.. js/document -location -href) "#/new"))}]
+             [:table
+              [:tr
+               [:th "Полное имя"]
+               [:th "Пол"]
+               [:th "Дата рождения"]
+               [:th ""]
+               [:th ""]]
+              patients-list]])))})))
