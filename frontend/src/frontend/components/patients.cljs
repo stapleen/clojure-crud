61660f183e737649a5d058a492eb38aca002f123
Mikhail <mm.leontev@gmail.com>
2020-10-05 13:17:37 +0300

    material-ui table

--- frontend/src/frontend/components/patients.cljs
+++ frontend/src/frontend/components/patients.cljs
@@ -7,17 +7,19 @@
    [moment :as moment]
    [reagent-material-ui.core.circular-progress :refer [circular-progress]]
    [frontend.config :as config]
-  ;;  [reagent-material-ui.core.table-body :refer [table-body]]
-  ;;  [reagent-material-ui.core.table-cell :refer [table-cell]]
-  ;;  [reagent-material-ui.core.table-container :refer [table-container]]
-  ;;  [reagent-material-ui.core.table-head :refer [table-head]]
-  ;;  [reagent-material-ui.core.table-row :refer [table-row]]
-   ))
+   [reagent-material-ui.core.table :refer [table]]
+   [reagent-material-ui.core.table-body :refer [table-body]]
+   [reagent-material-ui.core.table-cell :refer [table-cell]]
+   [reagent-material-ui.core.table-container :refer [table-container]]
+   [reagent-material-ui.core.table-head :refer [table-head]]
+   [reagent-material-ui.core.table-row :refer [table-row]]
+   [reagent-material-ui.styles :as styles]
+   [reagent-material-ui.core.paper :refer [paper]]))
 
 (defn component
   []
   (let [patients (r/atom nil)
-        loading (r/atom true)]
+        loading? (r/atom true)]
 
     (r/create-class
      {:display-name  "patients"
@@ -27,20 +29,9 @@
         (go (let [response (<! (http/get (str config/url "/get")))
                   result (get-in response [:body :result])]
               (reset! patients result)
-              (reset! loading false))))
+              (reset! loading? false))))
 
       :reagent-render
-      ;; (fn []
-
-
-
-
-
-      ;;   [:table-container
-      ;;    [:table
-      ;;     [:table-head
-      ;;      [:table-cell {:value "Имя"}]]]])
-
       (fn []
         (let [patients-list
               (map #(let [id (get-in % [:id])
@@ -48,46 +39,48 @@
                           gender (get-in % [:gender])
                           date_of_birth (get-in % [:date_of_birth])
                           date-formated (.format  (moment. date_of_birth) "DD.MM.YYYY")]
-
-                      [:tr {:key id}
-                       [:td full-name]
-                       [:td gender]
-                       [:td date-formated]
-                       [:td [:input {:type "button"
-                                     :value "Редактировать"
-                                     :on-click (fn []
-                                                 (set! (.. js/document -location -href) (str "#/edit/" id)))}]]
-                       [:td [:input {:type "button"
-                                     :value "Удалить"
-                                     :on-click
-                                     (fn []
-                                       (go (let [response (<! (http/post (str config/url "/delete")
-                                                                         {:json-params {:id id}}))
-                                                 success (get-in response [:body :success])]
-                                             (if (zero? success)
-                                               (js/alert (get-in response [:body :error]))
-                                               (reset! patients
-                                                       (filterv
-                                                        (fn [x] (not= (get-in x [:id]) id)) @patients))))))}]]])
+                      [table-row {:key id}
+                       [table-cell full-name]
+                       [table-cell gender]
+                       [table-cell date-formated]
+                       [table-cell
+                        [:div
+                         [:input {:type "button"
+                                  :value "Редактировать"
+                                  :on-click (fn []
+                                              (set! (.. js/document -location -href) (str "#/edit/" id)))}]]
+                        [:input {:type "button"
+                                 :value "Удалить"
+                                 :on-click
+                                 (fn []
+                                   (go (let [response (<! (http/post (str config/url "/delete")
+                                                                     {:json-params {:id id}}))
+                                             success (get-in response [:body :success])]
+                                         (if (zero? success)
+                                           (js/alert (get-in response [:body :error]))
+                                           (reset! patients
+                                                   (filterv
+                                                    (fn [x] (not= (get-in x [:id]) id)) @patients))))))}]]])
                    @patients)]
 
-          (if (true? @loading) [circular-progress {:color "secondary"}]
+          (if (true? @loading?) [circular-progress {:color "secondary"}]
               (if (empty? patients-list)
                 [:div
                  [:input {:type "button"
                           :value "Добавить пациента"
                           :on-click (fn [] (set! (.. js/document -location -href) "#/new"))}]
                  [:p "Список пациентов пуст"]]
-                [:div
+                [paper
                  [:input {:type "button"
                           :value "Добавить пациента"
                           :on-click (fn [] (set! (.. js/document -location -href) "#/new"))}]
-                 [:table
-                  [:tr
-                   [:th "Полное имя"]
-                   [:th "Пол"]
-                   [:th "Дата рождения"]
-                   [:th ""]
-                   [:th ""]]
-                  patients-list]]))
-                  ))})))
+                 [table-container
+                  [table
+                   [table-head
+                    [table-row
+                     [table-cell "Полное имя"]
+                     [table-cell "Пол"]
+                     [table-cell "Дата рождения"]
+                     [table-cell ""]]]
+                   [table-body
+                    patients-list]]]]))))})))
