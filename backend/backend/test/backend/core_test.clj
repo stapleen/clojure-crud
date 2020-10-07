c76571eb209be39f04b2daefca96514c45bf2c29
Mikhail <mm.leontev@gmail.com>
2020-10-07 15:21:17 +0300

    /update, /delete testing

--- backend/backend/test/backend/core_test.clj
+++ backend/backend/test/backend/core_test.clj
@@ -17,8 +17,8 @@
       (add-patients request)
       (let [result (jdbc/query db ["SELECT full_name FROM patients WHERE full_name='unique_test_name'"])
             full-name (get-in (first result) [:full_name])]
-        (is (= "unique_test_name" full-name))
-        (jdbc/delete! db :patients ["full_name = 'unique_test_name'"])))))
+        (jdbc/delete! db :patients ["full_name = 'unique_test_name'"])
+        (is (= "unique_test_name" full-name))))))
 
 (deftest insert-patient-test-2
   (testing "insert-patient-test-2"
@@ -26,23 +26,51 @@
       (add-patients request)
       (let [result (jdbc/query db ["SELECT full_name FROM patients WHERE full_name='unique_test_name-null'"])
             full-name (get-in (first result) [:full_name])]
-        (is (= nil full-name))
-        (jdbc/delete! db :patients ["full_name = 'unique_test_name-null'"])))))
-
-(def patient (jdbc/insert! db :patients {:full_name "Test"
-                                         :gender "М"
-                                         :date_of_birth (convert-string-to-date "2020-01-01")}))
-(def id-patient (get-in (first patient) [:id]))
+        (jdbc/delete! db :patients ["full_name = 'unique_test_name-null'"])
+        (is (= nil full-name))))))
 
 (deftest select-patients-test
   (testing "select-patients-test"
+    (jdbc/insert! db :patients {:full_name "Test"
+                                :gender "М"
+                                :date_of_birth (convert-string-to-date "2020-01-01")})
     (let [result (get-in (get-patients []) [:body :result])]
+      (jdbc/delete! db :patients ["full_name = 'Test'"])
       (is (= false (empty? result))))))
 
 (deftest select-patient-test
   (testing "select-patient-test"
-    (let [request {:body {"id" id-patient}}
+    (let [patient (jdbc/insert! db :patients {:full_name "Test"
+                                              :gender "М"
+                                              :date_of_birth (convert-string-to-date "2020-01-01")})
+          id-patient (get-in (first patient) [:id])
+          request {:body {"id" id-patient}}
           result (get-in (get-patient request) [:body :result])]
+      (jdbc/delete! db :patients ["id = ?" id-patient])
       (is (= false (empty? result))))))
 
-;; (jdbc/delete! db :patients ["id = ?" id-patient])
+(deftest update-patient-test
+  (testing "update-patient-test"
+    (let [patient (jdbc/insert! db :patients {:full_name "Test"
+                                              :gender "М"
+                                              :date_of_birth (convert-string-to-date "2020-01-01")})
+          id-patient (get-in (first patient) [:id])
+          request {:body {"id" id-patient, "full_name" "changed-name"}}
+          update-result (get-in (update-patient-data request) [:body :result])
+          select-result (jdbc/query db ["SELECT full_name FROM patients WHERE id = ?" id-patient])
+          name (get-in (first select-result) [:full_name])]
+      (jdbc/delete! db :patients ["id = ?" id-patient])
+      (is (= "changed-name" name)))))
+
+(deftest delete-patient-test
+  (testing "delete-patient-test"
+    (let [patient (jdbc/insert! db :patients {:full_name "Test"
+                                              :gender "М"
+                                              :date_of_birth (convert-string-to-date "2020-01-01")})
+          id-patient (get-in (first patient) [:id])
+          request {:body {"id" id-patient}}
+          delete-patient (get-in (delete-patient request) [:body :result])
+          select-result (jdbc/query db ["SELECT deleted FROM patients WHERE id = ?" id-patient])
+          result (get-in (first select-result) [:deleted])]
+      (jdbc/delete! db :patients ["id = ?" id-patient])
+      (is (= true result)))))
